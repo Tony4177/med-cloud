08dfbc8e43e3e07dc89fa0087a5442334f2d34c0
Tony4177 <bollepalliakhil3131@gmail.com>
2026-02-28 00:20:30 +0530

    app.py

--- app.py
+++ app.py
@@ -168,3 +168,4 @@ elif st.session_state.page == "create_account":
         st.success(f"New ID: {st.session_state.generated_id}")
         if st.button("Go to Sign In"): st.session_state.page = "auth"; st.rerun()
 
+
